a7fc84da7b0a02e3fb50043220ee2495980d9963
Matthew Caseres <matthewcaseres@outlook.com>
2020-12-19 13:08:13 -0600

    Update getGhRawUrl.ts

--- src/makeConfig/getGhRawUrl.ts
+++ src/makeConfig/getGhRawUrl.ts
@@ -1,15 +1,16 @@
 import GitUrlParse from 'git-url-parse';
 
 export default function getGhRawUrl(gitHubUrl: string, rawProvider: string) {
-  const { full_name, ref, filepath } = GitUrlParse(gitHubUrl);
-  const directory = filepath
+  const { full_name, pathname } = GitUrlParse(gitHubUrl);
+  const dir = pathname
     .split('/')
     .slice(0, -1)
     .join('/');
-  const ghPrefix = `https://github.com/${full_name}/blob/${ref}/${directory &&
-    directory + '/'}`;
-  const rawPrefix = `${rawProvider}${full_name}/${ref}/${directory &&
-    directory + '/'}`;
-  const rawSummaryUrl = `${rawPrefix}${filepath}`;
+  const ghPrefix = `https://github.com${dir + '/'}`;
+
+  const rawPrefix = `${rawProvider}${dir.replace('blob/', '') + '/'}`;
+  const rawSummaryUrl = `${rawProvider}${pathname.replace('blob/', '')}`;
   return { ghPrefix, rawPrefix, full_name, rawSummaryUrl };
 }
+
+
