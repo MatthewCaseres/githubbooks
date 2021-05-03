3b2855c4acd88df7544dd9b9e72ae553cebb60ec
Matthew Caseres <matthewcaseres@outlook.com>
2021-05-03 13:42:35 -0500

    booolin

--- test/getAllRoutesInfo.test.ts
+++ test/getAllRoutesInfo.test.ts
@@ -1,24 +1,28 @@
 import { getAllRoutesInfo } from '../src/getStatic';
 import jsons from '../testData/00-JSONs.json'
+import fs from 'fs'
 
 test('works for FP guide (root config, same level)', () => {
   const res = getAllRoutesInfo(jsons);
+  fs.writeFileSync('lol.json', JSON.stringify(res))
   expect(res).toEqual({
-    'Open-EdTech/next-mdx-books/file1.md': {
-      rawUrl: 'https://raw.githubusercontent.com/Open-EdTech/next-mdx-books/main/testData/file1.md',
-      ghUrl: 'https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/file1.md',
-      route: 'Open-EdTech/next-mdx-books/file1.md',
-      path: undefined,
-      index: 0,
-      treePath: [ 0 ]
+    "Open-EdTech/next-mdx-books/file1.md": {
+      "title": "lol1",
+      "rawUrl": "https://raw.githubusercontent.com/Open-EdTech/next-mdx-books/main/testData/file1.md",
+      "ghUrl": "https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/file1.md",
+      "route": "Open-EdTech/next-mdx-books/file1.md",
+      "index": 0,
+      "treePath": [0],
+      "next": { "title": "lol2", "route": "Open-EdTech/next-mdx-books/file2.md" }
     },
-    'Open-EdTech/next-mdx-books/file2.md': {
-      rawUrl: 'https://raw.githubusercontent.com/Open-EdTech/next-mdx-books/main/testData/file2.md',
-      ghUrl: 'https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/file2.md',
-      route: 'Open-EdTech/next-mdx-books/file2.md',
-      path: undefined,
-      index: 0,
-      treePath: [ 1 ]
+    "Open-EdTech/next-mdx-books/file2.md": {
+      "title": "lol2",
+      "rawUrl": "https://raw.githubusercontent.com/Open-EdTech/next-mdx-books/main/testData/file2.md",
+      "ghUrl": "https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/file2.md",
+      "route": "Open-EdTech/next-mdx-books/file2.md",
+      "index": 0,
+      "treePath": [1],
+      "prev": { "title": "lol1", "route": "Open-EdTech/next-mdx-books/file1.md" }
     }
   })
 });
--- test/summaryToUrlTree.test.ts
+++ test/summaryToUrlTree.test.ts
@@ -1,42 +1,40 @@
-// import {summaryToUrlTree} from '../src/makeConfig/summaryToUrlTree';
-// import JSON00 from '../testData/00-JSON.json';
-// import path from 'path';
-// import fs from 'fs';
-// import visit from 'unist-util-visit';
-// import GithubSlugger from 'github-slugger';
-// import { UserFunction } from '../src';
+import {summaryToUrlTree} from '../src/makeConfig/summaryToUrlTree';
+import JSON00 from '../testData/00-JSON.json';
+import path from 'path';
+import visit from 'unist-util-visit';
+import GithubSlugger from 'github-slugger';
+import { UserFunction } from '../src';
 
-// //Let the user add a function
-// const userFunction: UserFunction = (node, { mdast, frontMatter }) => {
-//   const routePrefix = node.route;
-//   var slugger = new GithubSlugger();
-//   let headers: any[] = [];
-//   visit(mdast, 'heading', (mdNode: any) => {
-//     if (mdNode.depth === 2) {
-//       let header: any = {};
-//       header.title = mdNode.children[0].value;
-//       header.route = routePrefix + '/#' + slugger.slug(header.title);
-//       header.type = 'heading';
-//       headers.push(header);
-//     }
-//   });
-//   if (headers.length > 0) {
-//     node.headers = headers
-//   }
-//   if( Object.keys(frontMatter).length > 0) {
-//     node.frontMatter = frontMatter
-//   }
-// };
+//Let the user add a function
+const userFunction: UserFunction = ({ treeNode, mdast, frontMatter }) => {
+  const routePrefix = treeNode.route;
+  var slugger = new GithubSlugger();
+  let headers: any[] = [];
+  visit(mdast, 'heading', (mdNode: any) => {
+    if (mdNode.depth === 2) {
+      let header: any = {};
+      header.title = mdNode.children[0].value;
+      header.route = routePrefix + '/#' + slugger.slug(header.title);
+      header.type = 'heading';
+      headers.push(header);
+    }
+  });
+  if (headers.length > 0) {
+    treeNode.headers = headers
+  }
+  if( Object.keys(frontMatter).length > 0) {
+    treeNode.frontMatter = frontMatter
+  }
+};
 
-// test('Constructs valid URL tree', async () => {
-//   const result = await summaryToUrlTree({
-//     url:
-//       'https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/TOC.md',
-//     localPath: path.join(__dirname, '..', 'testData', 'TOC.md'),
-//     userFunction: userFunction,
-//     rawProvider: 'https://raw.githubusercontent.com',
-//   });
-//   fs.writeFileSync('hmm.json', JSON.stringify(result));
+test('Constructs valid URL tree', async () => {
+  const result = await summaryToUrlTree({
+    url:
+      'https://github.com/Open-EdTech/next-mdx-books/blob/main/testData/TOC.md',
+    localPath: path.join(__dirname, '..', 'testData', 'TOC.md'),
+    userFunction: userFunction,
+    rawProvider: 'https://raw.githubusercontent.com',
+  });
 
-//   expect(result).toEqual(JSON00);
-// });
+  expect(result).toEqual(JSON00);
+});
